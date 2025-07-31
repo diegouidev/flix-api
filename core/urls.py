eeee516940af6b44f0d70cb132180e7cb161da97
diegouidev <diegouidev@gmail.com>
2025-07-31 20:12:02 -0300

    crud app actors

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreRetrieveUpdateDestroyView, GenreListCreateView
+from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres', GenreListCreateView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
+
+    path('actors/', ActorListCreateView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail'),
+
 ]
