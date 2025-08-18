ddd5e07c8a914ed4521aee7002994428afc0063c
diegouidev <diegocoelho44@gmail.com>
2025-08-18 16:53:22 -0300

    adicionando permissoes

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Movies
 from .serializers import MoviesSerializer
 
 class MoviesListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movies.objects.all()
     serializer_class = MoviesSerializer
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movies.objects.all()
     serializer_class = MoviesSerializer
