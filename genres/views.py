4b4699e753939825510e4b061fcb1744b64ab912
diegouidev <diegocoelho44@gmail.com>
2025-08-27 16:12:58 -0300

    Criando classe de permissão

--- genres/views.py
+++ genres/views.py
@@ -2,10 +2,10 @@ from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from rest_framework import generics
 from genres.serializer import GenreSerializer
-
+from genres.permissions import GenrePermissionClass
 
 class GenreListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
