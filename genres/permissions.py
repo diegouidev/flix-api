5b2437aa1a54b532d46b4c15307e71aba6f5ba2b
diegouidev <diegocoelho44@gmail.com>
2025-09-02 10:32:12 -0300

    Criando classe global de permissões

--- genres/permissions.py
+++ genres/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 
 class GenrePermissionClass(permissions.BasePermission):
+    
     def has_permission(self, request, view):
         if request.method in ['GET', 'OPTIONS', 'HEAD']:
             return request.user.has_perm('genres.view_genre')
